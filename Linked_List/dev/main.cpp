44913814e6f6cf97f5c4b06536619d44a2e81623
adusachev <usachev.alexe@gmail.com>
2022-10-11 09:12:51 +0300

    init version of linked list

--- Linked_List/dev/main.cpp
+++ Linked_List/dev/main.cpp
@@ -68,71 +68,9 @@ struct DoublyLinkedList {
 		cout << endl;
 	};
 
-	////////////////////////////////////////////////////////
+	
 
 
-
-
-
-	void clear() {
-		while (first != nullptr) {
-			Node* tmpNode = first->next;
-			delete first;
-			first = tmpNode;
-		}
-		size_ = 0;
-	}
-
-	void pop_front() {
-		if (first != nullptr) {
-			Node* tmpNode = first->next;
-			delete first;
-			first = tmpNode;
-			size_--;
-		}
-	}
-
-	bool is_empty() {
-		if (first != nullptr) {
-			return true;
-		}
-		return false;
-	}
-
-	void insert_after(int position, int value) {
-		Node* curNode = first;
-		int pos = 0;
-		while (!(curNode == nullptr))
-		{
-			if (position == pos) {
-				Node* tmpNode = new Node;
-				tmpNode->value = value;
-
-				tmpNode->next = curNode->next;
-
-				curNode->next = tmpNode;
-
-				size_++;
-				return;
-			}
-			pos++;
-			curNode = curNode->next;
-		}
-	}
-
-	int operator[] (const int index) {
-		Node* curNode = first;
-		int pos = 0;
-		while (!(curNode == nullptr))
-		{
-			if (index == pos) {
-				return curNode->value;
-			}
-			pos++;
-			curNode = curNode->next;
-		}
-		std::cout << "Out of range";
-	}
 
 	int size() {
 		return size_;
@@ -148,29 +86,9 @@ struct DoublyLinkedList {
 
 int main() {
    
-    DoublyLinkedList list;
-
-    list.push_back(10);
-	list.push_back(5);
-    list.push_back(7);
-    list.push_back(-19);
-
-	//list.push_front(10);
-	//list.push_front(5);
-	//list.push_front(7);
-	//list.push_front(-19);
-
-	list.print();
-	list.print_reverse();
+    
 
 
-
-    //list.insert_after(0, 20);
-
-    //list.print();
-	//cout << "Size: " << list.size();
-
-	//list.print_reverse();
                                                                                 
     return 0;
 }
