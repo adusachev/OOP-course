536aed0bc6753d77b2353efeae7769968b36f249
adusachev <usachev.alexe@gmail.com>
2022-10-14 11:45:40 +0300

    created week3 project

---
+++ week_3/dev/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+
+
+
+
+
+
+int main() {
+   
+    
+                                                                                
+    return 0;
+}
+
+
